1975df1ea1f942ba03fba839fdad061250c4bfb4
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-15 18:37:17 +0600

    core build.gradle updated

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(Plugins.library)
+    id(Plugins.android)
 }
 
 android {
     namespace = "com.zhigaras.core"
-    compileSdk = 33
+    compileSdk = Config.compileSdk
     
     defaultConfig {
-        minSdk = 24
+        minSdk = Config.minSdk
         
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = Config.testInstrumentationRunner
         consumerProguardFiles("consumer-rules.pro")
     }
     
@@ -24,20 +24,21 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = Config.jvmTarget
     }
 }
 
 dependencies {
     
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appcompat)
+    implementation(Dependencies.material)
+    
+    testImplementation(Dependencies.jUnit)
+    androidTestImplementation(Dependencies.androidJUnit)
+    androidTestImplementation(Dependencies.espresso)
 }
