c503b6d0e2985ecc09209efe102ac66e06d326a5
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-16 05:48:24 +0600

    RatingView created

---
+++ uiKit/src/main/java/com/zhigaras/uikit/RatingView.kt
@@ -0,0 +1,14 @@
+package com.zhigaras.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class RatingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): AppCompatTextView(context, attrs, defStyleAttr) {
+    
+
+}
