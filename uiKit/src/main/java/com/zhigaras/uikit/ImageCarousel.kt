2a5706267157b689ce816ed54f273c3968da3925
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-17 03:04:27 +0600

    carousel size fixed

--- uiKit/src/main/java/com/zhigaras/uikit/ImageCarousel.kt
+++ uiKit/src/main/java/com/zhigaras/uikit/ImageCarousel.kt
@@ -2,7 +2,7 @@ package com.zhigaras.uikit
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import androidx.cardview.widget.CardView
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -11,7 +11,7 @@ class ImageCarousel @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : FrameLayout(context, attrs, defStyleAttr) {
+) : CardView(context, attrs, defStyleAttr) {
     
     private val viewPager by lazy { findViewById<ViewPager2>(R.id.view_pager) }
     private val tabLayout by lazy { findViewById<TabLayout>(R.id.tab_layout) }
