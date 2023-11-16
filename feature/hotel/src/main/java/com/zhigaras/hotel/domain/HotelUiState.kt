45db0da9187231c48d5ca11d3ca7aae124c71fdd
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-17 05:44:26 +0600

    progress state added

--- feature/hotel/src/main/java/com/zhigaras/hotel/domain/HotelUiState.kt
+++ feature/hotel/src/main/java/com/zhigaras/hotel/domain/HotelUiState.kt
@@ -1,5 +1,6 @@
 package com.zhigaras.hotel.domain
 
+import android.view.View
 import com.zhigaras.core.UiState
 import com.zhigaras.hotel.databinding.FragmentHotelBinding
 import com.zhigaras.hotel.ui.model.HotelDetailsUiModel
@@ -11,19 +12,23 @@ interface HotelUiState : UiState<FragmentHotelBinding> {
     }
     
     class Loading : HotelUiState {
-        override fun update(binding: FragmentHotelBinding) {
-        
+        override fun update(binding: FragmentHotelBinding) = with(binding) {
+            payload.visibility = View.GONE
+            progressBar.visibility = View.VISIBLE
         }
     }
     
     class Success(private val data: HotelDetailsUiModel) : HotelUiState {
-        override fun update(binding: FragmentHotelBinding) {
-            data.bindAboutHotel(binding.aboutHotelLayout)
-            data.bindHotelDetails(binding.hotelDetailsLayout)
+        override fun update(binding: FragmentHotelBinding) = with(binding) {
+            data.bindAboutHotel(aboutHotelLayout)
+            data.bindHotelDetails(hotelDetailsLayout)
+            payload.visibility = View.VISIBLE
+            progressBar.visibility = View.GONE
         }
     }
     
     class Error(private val message: String) : HotelUiState {
+        
         override fun update(binding: FragmentHotelBinding) {
         
         }
