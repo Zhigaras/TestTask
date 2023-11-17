cbe564af7b704ee4e48197586b4f99ced610df63
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-17 08:33:22 +0600

    navBar methods added

--- uiKit/src/main/java/com/zhigaras/uikit/NavBar.kt
+++ uiKit/src/main/java/com/zhigaras/uikit/NavBar.kt
@@ -3,11 +3,20 @@ package com.zhigaras.uikit
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
 
 class NavBar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-): FrameLayout(context, attrs, defStyleAttr) {
-
+) : FrameLayout(context, attrs, defStyleAttr) {
+    
+    fun setHeader(header: String) {
+        findViewById<TextView>(R.id.nav_bar_title).text = header
+    }
+    
+    fun setBackClickListener(listener: OnClickListener) {
+        findViewById<ImageView>(R.id.back_button).setOnClickListener(listener)
+    }
 }
