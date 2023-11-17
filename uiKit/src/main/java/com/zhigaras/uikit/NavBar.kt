000e745bd419bf287d8ae1bf9f289029aabb5fd1
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-17 08:08:42 +0600

    nav bar created

---
+++ uiKit/src/main/java/com/zhigaras/uikit/NavBar.kt
@@ -0,0 +1,13 @@
+package com.zhigaras.uikit
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class NavBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+): FrameLayout(context, attrs, defStyleAttr) {
+
+}
