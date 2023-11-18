841b3a6aa5a2f0b1413432b2c8ce41fb97910697
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-19 04:10:25 +0600

    ordered module created

---
+++ feature/ordered/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id(Plugins.library)
+    id(Plugins.android)
+}
+
+android {
+    namespace = "com.zhigaras.ordered"
+    compileSdk = Config.compileSdk
+    
+    defaultConfig {
+        minSdk = Config.minSdk
+        
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+}
+
+dependencies {
+    
+    implementation(project(":core"))
+    implementation(project(":uiKit"))
+    
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appcompat)
+    implementation(Dependencies.material)
+    implementation(Dependencies.koinAndroid)
+}
