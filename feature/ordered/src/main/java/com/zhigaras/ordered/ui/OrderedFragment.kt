e744c2eb488ad5aa4fcf96bb6d1e6416eeec4318
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-19 05:50:14 +0600

    back press handled on ordered screen

--- feature/ordered/src/main/java/com/zhigaras/ordered/ui/OrderedFragment.kt
+++ feature/ordered/src/main/java/com/zhigaras/ordered/ui/OrderedFragment.kt
@@ -3,6 +3,7 @@ package com.zhigaras.ordered.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import com.zhigaras.core.BaseFragment
 import com.zhigaras.ordered.R
 import com.zhigaras.ordered.databinding.FragmentOrderedBinding
@@ -16,8 +17,16 @@ class OrderedFragment : BaseFragment<FragmentOrderedBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() = viewModel.goToStartScreen()
+            })
+        
         binding.navBar.backButton.setOnClickListener { viewModel.goToStartScreen() }
         binding.navBar.navBarTitle.setText(R.string.order_has_been_paid)
-        binding.bottomButton.button.setOnClickListener { viewModel.goToStartScreen() }
+        binding.bottomButton.button.apply {
+            setOnClickListener { viewModel.goToStartScreen() }
+            setText(R.string.super_text)
+        }
     }
 }
