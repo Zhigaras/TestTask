73548843c2598e2c40234115b305cb5e885e004d
Ilya Zhigaras <zhigaras.dev@gmail.com>
2023-11-18 06:30:41 +0600

    tourist info header fixed

--- feature/booking/src/main/java/com/zhigaras/booking/ui/TouristInfoView.kt
+++ feature/booking/src/main/java/com/zhigaras/booking/ui/TouristInfoView.kt
@@ -5,6 +5,7 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.zhigaras.booking.R
+import com.zhigaras.booking.databinding.TouristInfoHeaderBinding
 
 class TouristInfoView @JvmOverloads constructor(
     context: Context,
@@ -13,8 +14,11 @@ class TouristInfoView @JvmOverloads constructor(
 ) : LinearLayout(context, attrs, defStyleAttr) {
     
     private val hints = resources.getStringArray(R.array.tourist_info)
+    private val headerBinding =
+        TouristInfoHeaderBinding.inflate(LayoutInflater.from(context), this, false)
     
     init {
+        addView(headerBinding.root)
         hints.forEach {
             val inputView = LayoutInflater.from(context).inflate(R.layout.input_layout, this, false)
             (inputView as BuyerInfoInput).hint = it
